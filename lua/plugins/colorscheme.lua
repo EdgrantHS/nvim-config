5908e7290cc218581652564e294923c2b10c8acf
EdgrantHS <edgrant.h@gmail.com>
2025-04-28 10:27:02 +0700

    init

---
+++ lua/plugins/colorscheme.lua
@@ -0,0 +1,26 @@
+return {
+  {
+    "catppuccin/nvim",
+    name = "catppuccin",
+    opts = {
+      transparent_background = true,
+      flavour = "frappe",
+      styles = {
+        comments = { "italic" },
+      },
+      -- color_overrides = {
+      --   all = {
+      --     overlay0 = "#C0C0C0",
+      --   },
+      -- },
+    },
+    priority = 1000,
+    lazy = false,
+  },
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "catppuccin",
+    },
+  },
+}
